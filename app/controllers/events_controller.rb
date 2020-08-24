b92a47cf96d8f8aceacbc5c99f9ced166af2d71a
yuta <fantasista098@gmail.com>
2020-08-24 22:49:50 +0900

    詳細ページの実装、レイアウトの調整を実施

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   def show
     if params[:id].present?
       @event = Event.find(params[:id])
+      @subsidies = @event.subsidies.order(start_date: :desc).page(params[:page]).per(12)
     else
       redirect_to root_path
     end
