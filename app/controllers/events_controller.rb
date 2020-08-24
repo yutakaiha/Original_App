5a46768ba4109e286e88638bba80ba9911e44f67
yuta <fantasista098@gmail.com>
2020-08-24 18:01:00 +0900

    showコントローラーの仮定義、共通ヘッダーのドロップダウンへのリンクを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,9 @@
 class EventsController < ApplicationController
   def show
+    if params[:id].present?
+      @event = Event.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 end
