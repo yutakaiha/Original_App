b76b8ba694c3121a8a3efac37a78b2cb81d01396
yuta <fantasista098@gmail.com>
2020-08-27 23:15:35 +0900

    一覧ページのコントローラーの実装及びビューないの条件分岐による表示を実施

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,9 @@ class QuestionsController < ApplicationController
   def index
     if params[:category_id].present?
       @category = Category.find(params[:category_id])
-      @caquestions = @category.questionos.order(created_at: :desc).page(params[:page]).per(7)
+      @questions = @category.questionos.order(created_at: :desc).page(params[:page]).per(7)
     else
-
+      @all_questions = Question.order(created_at: :desc).page(params[:page]).per(7)
     end
-    @questions = Question.order(created_at: :desc)
   end
 end
