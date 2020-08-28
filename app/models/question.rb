181d3343f52e6a720c21e4f79061e8dc3c18a749
yuta <fantasista098@gmail.com>
2020-08-28 22:26:53 +0900

    一覧ページに検索機能を実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :category
   is_impressionable counter_cache: true
+
+  def self.ransackable_attributes(auth_object = nil)
+      %w[title]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+      []
+  end
 end
