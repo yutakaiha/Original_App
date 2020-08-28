e992ffc7117af32b3c24e821329a5f315cf1ebac
yuta <fantasista098@gmail.com>
2020-08-28 09:26:51 +0900

    シードデータの作成・投入、質問一覧ページとカテゴリ一覧ページのレイアウト共通化

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   def index
     if params[:category_id].present?
       @category = Category.find(params[:category_id])
-      @questions = @category.questionos.order(created_at: :desc).page(params[:page]).per(7)
+      @questions = @category.questions.order(created_at: :desc).page(params[:page]).per(7)
     else
       @all_questions = Question.order(created_at: :desc).page(params[:page]).per(7)
     end
