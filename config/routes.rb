ff7fd1b68e48c4f1ac286a61109924d63a26f3aa
yuta <fantasista098@gmail.com>
2020-09-01 19:20:17 +0900

    ルーティグの設定

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :answers, only: [:new, :create, :destroy] do
       resource :best_answer, only: [:show, :create, :destroy]
       resource :favorite, only: [:create, :destroy]
+      resource :comments, only: [:new, :create, :destroy]
     end
   end
 
