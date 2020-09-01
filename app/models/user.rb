9ed640310e728c1050ee88f3eacea5c74a01170f
yuta <fantasista098@gmail.com>
2020-09-01 19:32:21 +0900

    User,Answer,Commentモデルの関連付けを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_answers, through: :favorites, source: :answer
-
+  has_many :comments, dependent: :destroy
+  has_many :commented_answers, through: :comments, source: :answer
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
