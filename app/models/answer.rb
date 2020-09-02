02ce2d6f96cc195b4d14062bb1ab29cce680c972
yuta <fantasista098@gmail.com>
2020-09-02 11:08:53 +0900

    questionモデルとanswerモデルのバリデーションを定義

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,8 @@ class Answer < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :commentting_users, through: :comments, source: :user
-
+  validates :content, presence: true, length: { maximum: 1000, too_long: "最大#{count}文字まで扱えます" }
+  
 
   scope :best_answer, -> (question) {joins(:question).merge(Question.answer_check(question))}
   
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,13 +3,18 @@ class Question < ApplicationRecord
   belongs_to :category
   has_many :answers, dependent: :destroy
   is_impressionable counter_cache: true
+  with_options presence: true do
+    validates :title
+    validates :content, length: {maximum: 1000, too_long: "最大#{count}文字まで扱えます"}
+    validates :user_id
+    validates :category_id
+  end
+
 
   # ベストアンサー（relationオブジェクトを返す） 
   # nil、falseの場合allがallクエリメソッドが適用されるためビュー側で条件分岐
   scope :answer_check, ->(question) {where("answers.id = ?", question.best_answer_id)}
   
-  
-
   
   def self.ransackable_attributes(auth_object = nil)
       %w[title]
