ac0aceddcd143b408b1dbe8424ac6550e167c804
yuta <fantasista098@gmail.com>
2020-09-04 18:31:27 +0900

    Userモデルからomniauthableの記述のコメントアウトを解除。またomniauth用のカラムをuserモデルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_101341) do
+ActiveRecord::Schema.define(version: 2020_09_04_092947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,10 @@ ActiveRecord::Schema.define(version: 2020_09_01_101341) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "nickname"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
