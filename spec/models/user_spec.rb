8a6c443c49b12f0fba423fed7b70377ec4aece88
yuta <fantasista098@gmail.com>
2020-09-04 11:44:49 +0900

    userモデルに基本的なバリデーションを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe "User バリデーションチェック" do
+    context "nicknameのバリデーション" do
+      let(:user){build(:user, nickname: "")}
+      it "nicknameが空欄の場合、User作成不可" do
+        expect(user.valid?).to eq false  
+      end
+    end
+    
+    context "emailのバリデーション" do
+      let(:user1){build(:user, email: "")}
+      let(:user2){create(:user)}
+      let(:test_user){build(:user, email: "foofoo")}
+
+      it "emailが空の場合、User作成不可" do
+        expect(user1.valid?).to eq false  
+      end
+      
+      it "emailが重複している場合" do
+        user2
+        expect(test_user.valid?).to eq false 
+      end
+    end
+    
+  end
 end
