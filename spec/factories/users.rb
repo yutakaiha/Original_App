0ad4407b2eadd389dc03d202bac213774b2c8f40
yuta <fantasista098@gmail.com>
2020-09-04 12:38:54 +0900

    Userのモデルテスト一通り完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:nickname){ |n| "test_user#{n}"}
     sequence(:email){ |n| "test#{n}@example.com"}
     password{"foofoo"}
+    password_confirmation{"foofoo"}
 
   end
 end
