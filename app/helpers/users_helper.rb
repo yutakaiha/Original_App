f15f27e4cc2bb8bbaab2fb2db0313dc617b284de
yuta <fantasista098@gmail.com>
2020-09-06 08:57:50 +0900

    ベストアンサー率の実装及びレイアウト微調整

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def best_answer_rate(best_answers, total_answers)
     if total_answers != 0
-      (best_answers / total_answers * 100 ).round
+      (best_answers.to_f / total_answers.to_f * 100 ).to_f.round
     else
       "まだ回答が一つもありません。"
     end
