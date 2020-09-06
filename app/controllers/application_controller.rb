d3f459a7652799675b378b531aed362baf699677
yuta <fantasista098@gmail.com>
2020-09-06 13:34:59 +0900

    アカウント編集ページにbirthday,sex追加、マイページの表記も修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image, :birthday, :sex])
   end
 
 end
--- db/migrate/20200906024817_add_age_and_sex_columns_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddAgeAndSexColumnsToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :age, :integer
-    add_column :users, :sex, :integer
-  end
-end
