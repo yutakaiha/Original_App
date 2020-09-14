ed768f43144e23962bc220477297ed686404f6e7
yuta <fantasista098@gmail.com>
2020-09-14 15:18:20 +0900

    サイドバーやリストバーの修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def show
     if params[:id].present?
       @event = Event.find(params[:id])
-      @subsidies = @event.subsidies.order(start_date: :desc).page(params[:page]).per(12)
+      @subsidies = @event.subsidies.order(start_date: :desc).page(params[:page]).per(8)
     else
       redirect_to root_path
     end
--- app/controllers/subsidies_controller.rb
+++ app/controllers/subsidies_controller.rb
@@ -1,7 +1,7 @@
 class SubsidiesController < ApplicationController
   def index
     @search_subsidy = Subsidy.ransack(params[:q])
-    @subsidies = @search_subsidy.result.page(params[:page]).per(16)
+    @subsidies = @search_subsidy.result.page(params[:page]).per(8)
   end
 
   def show
